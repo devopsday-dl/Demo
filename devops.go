1bb40bc9cd6b0185139d091a4f86c6a0bc9cc7dd
lhb008 <42570491+lhb008@users.noreply.github.com>
2020-04-16 14:54:32 +0800

    test

--- devops.go
+++ devops.go
@@ -18,7 +18,7 @@ func get_env_var() string {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello DevOps, This is xiaomage, pod name is %s, version is %s",get_hostname(), get_env_var())
+    fmt.Fprintf(w, "Hello DevSecOps, This is xiaomage, pod name is %s, version is %s",get_hostname(), get_env_var())
 }
 
 func main() {
