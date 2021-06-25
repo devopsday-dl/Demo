d564b04bfc229bf344c0191847961978f1f34bed
majinghe <42570491+majinghe@users.noreply.github.com>
2021-06-25 17:56:22 +0800

    Update devops.go

--- devops.go
+++ devops.go
@@ -4,24 +4,13 @@ import (
     "fmt"
     "log"
     "net/http"
-    "os"
 )
 
-func get_hostname() string {
-    hostname, _ := os.Hostname()
-    return hostname
-}
-
-// func get_env_var() string {
-//     version := os.Getenv("VERSION")
-//     return version
-// }
-
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello GitOps,this is xiaomage,version is v1, pod name is %s",get_hostname())
+    fmt.Fprintf(w, "Hello GitOps,this is xiaomage,version is v1.0.0")
 }
 
 func main() {
-    http.HandleFunc("/devopsday", handler)
+    http.HandleFunc("/gotc", handler)
     log.Fatal(http.ListenAndServe(":9999", nil))
 }
