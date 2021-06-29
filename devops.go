d64abaced269b3ddd611c448c60f2552572aea03
xiaomage-devops <majinghe@cn.ibm.com>
2021-06-29 10:52:23 +0800

    add environment vars

--- devops.go
+++ devops.go
@@ -4,10 +4,11 @@ import (
     "fmt"
     "log"
     "net/http"
+    "os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello GitOps,this is xiaomage,version is v1.0.0")
+    fmt.Fprintf(w, "Hello %s,this is %s,version is v1.0.1", os.Getenv("CONF"), os.Getenv("USERNAME"))
 }
 
 func main() {
